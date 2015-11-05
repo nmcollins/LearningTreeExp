86cd498cc6775f59ec0ac08303e97173d4bd2791
unknown <Ncollins9293@2A411-D06.conestogac.on.ca>
2015-11-05 11:29:08 -0500

    Start adding unit tests.

--- IntegrationLab/IntegrationLab/Form1.cs
+++ IntegrationLab/IntegrationLab/Form1.cs
@@ -12,9 +12,23 @@ namespace IntegrationLab
 {
     public partial class Form1 : Form
     {
+        public bool oldEnough = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public static bool checkAge(int age)
+        {
+            if (age > 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
 
         private void btnValidate_Click(object sender, EventArgs e)
@@ -22,10 +36,12 @@ namespace IntegrationLab
             if (Int32.Parse(textBox1.Text) > 18)
             {
                 MessageBox.Show("You're Old enough", "Validator");
+                oldEnough = true;
             }
             else
             {
                 MessageBox.Show("You're Too young", "Validator");
+
             }
         }
     }
---
+++ IntegrationLab/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool checkResult = IntegrationLab.Form1.checkAge(21);
+            Assert.AreEqual(true, checkResult);
+                
+        }
+    }
+}
